c27f3e31fab098e937e65f245b3b2777cd8b2767
GUOJUN <yui745698@gmail.com>
2020-05-28 16:30:15 +0800

    modify invite_guest_statistics page layout

--- bigday/urls.py
+++ bigday/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^admin/', admin.site.urls),
-    url('^accounts/', include('django.contrib.auth.urls'))
 ]
